6d3cd22bd6ef333afb741be5279e1adb25445da6
Taenam alzamil <92439253+taynamghz@users.noreply.github.com>
2025-03-19 14:35:40 +0300

    Create forwardAttack.ino

---
+++ 2025/methods/forwardAttack.ino
@@ -0,0 +1,40 @@
+
+void forward_attack() {
+  int edgeState = checkEdge();
+  // xmotion.Forward(100, 50); // %100 Speed, both motor forward 100mS.
+  // xmotion.StopMotors(0);
+  check_sensors(direction);
+if (direction == 1){
+   xmotion.Forward(100, 50); // %100 Speed, both motor forward 100mS.
+   //xmotion.Backward(100, 50); //                                          *** new (forward was 30 and no bw)
+  check_sensors(direction);
+
+}
+  // Continue forward while not crossing the edge and facing a front obstacle
+  while (direction == 1 ) {
+    // Read sensor values and update direction
+    check_sensors(direction);
+    Serial.println(" before attacking");
+
+    // Move forward
+    xmotion.Forward(100, 10); // %100 Speed, both motor forward 100mS.
+    Serial.println(" after attacking");
+
+    // After performing attack, check for edges
+    edgeState = checkEdge();
+    if (edgeState != 0){
+      // mayy not use may use 
+     // xmotion.Backward(100, 200);
+      break;
+    }
+  }
+  edgeState = checkEdge();
+  if (edgeState != 0) {
+      // If edge is detected, perform edge avoidance
+      avoidEdge(edgeState);
+      edgeState = checkEdge();
+      
+      }
+      
+      //avoidEdge(edgeState);
+    }
