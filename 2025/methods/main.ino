643d3c3fdddae04ff6275f42c2a0d642f97b0086
Taenam alzamil <92439253+taynamghz@users.noreply.github.com>
2025-04-06 22:10:01 +0300

    Update main.ino

--- 2025/methods/main.ino
+++ 2025/methods/main.ino
@@ -1,7 +1,48 @@
+#include <xmotionV3.h>
+#include "xmotionV3.h"
 
+
+int ML1 = A4; // fl Micro Line Sensor
+int ML2 = A1; // back
+int ML3 = A2; // right  Micro Line Sensor
+
+int sensorV[3] = {0, 0, 0};
+
+int sensorLF = 0;
+int sensorF = 1;
+int sensorRF = 2;
+int sensorL = 4;
+int sensorR = A5;
+
+
+int direction = 0;
+int edgeState;
+
+void setup() {
+  // Initialize Serial communication
+  Serial.begin(9600);
+
+  // Initialize setup if needed
+  pinMode(ML1, INPUT);
+  pinMode(ML3, INPUT);
+  pinMode(sensorLF, INPUT);
+  pinMode(sensorF, INPUT);
+  pinMode(sensorRF, INPUT);
+  pinMode(sensorL, INPUT);
+  pinMode(sensorR, INPUT);
+
+  Serial.begin(9600); //Serial Interface started with 9600 bits per sec.
+}
+
+void loop() {
+  
+int edgeState = checkEdge();
+if (edgeState != 0){
+avoidEdge(edgeState);    }
+  
 while (true){
 int edgeState = checkEdge();
-check_sensors(direction); // Update direction based on sensor readings
+checkOpponent(direction); // Update direction based on sensor readings
   if (edgeState == 0 && direction !=0 ) {
     Serial.println("NO EDGE & opponent detected");
     do {
@@ -10,22 +51,22 @@ check_sensors(direction); // Update direction based on sensor readings
         if (edgeState != 0){
         break;
          }
-        check_sensors(direction); // Update direction based on sensor readings
+        checkOpponent(direction); // Update direction based on sensor readings
     } while (direction != 0); // Exit loop if opponent is detected or edge is detected
     Serial.println("EXIT MAIN LOOP");
     avoidEdge(edgeState);
-    check_sensors(direction); // Update direction based on sensor readings 
+    checkOpponent(direction); // Update direction based on sensor readings 
 }
 if (direction == 0) {
     //hunt();
     edgeState = checkEdge();
     while (edgeState == 0 && direction == 0){
       xmotion.Forward(50, 10);
-      check_sensors(direction);
+      checkOpponent(direction);
       if (direction != 0){
         break;
       }
-      check_sensors(direction);
+      checkOpponent(direction);
       edgeState = checkEdge();
     }
     avoidEdge(edgeState);
@@ -35,10 +76,11 @@ if (direction == 0) {
     time = time + 10;
     if (time == 2000) {break;}
         //xmotion.ArcTurn(15, 30, 20);
-         check_sensors(direction); // Update direction based on sensor readings
+         checkOpponent(direction); // Update direction based on sensor readings
          edgeState = checkEdge();
       }    
    avoidEdge(edgeState);
   }
+}
 }
 }
