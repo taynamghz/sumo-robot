39773ed0b63754a3d8dda2b5dfb61d014c41df73
Taenam alzamil <92439253+taynamghz@users.noreply.github.com>
2025-04-10 17:36:40 +0300

    Update avoidEdge.ino

--- 2025/methods/avoidEdge.ino
+++ 2025/methods/avoidEdge.ino
@@ -1,79 +1,59 @@
-  void avoidEdge(int &edgeState) {
-  while (edgeState!=0)
+
+
+ void avoidEdge(int &edgeState) {
+while (edgeState!=0)
   {
 
 //move backward because both front sesnors are at edge 
-  if (edgeState=4){
+  if (edgeState==4){
   xmotion.Backward(100,400); 
-  xmotion.Left0(50,300);
+  xmotion.Left0(100,200);
   //xmotion.Forward(50,150);
   xmotion.StopMotors(0);
+          Serial.println("avoiding backward");
 
 
   edgeState = checkEdge();
 }
 //front right at edge 
-else if (edgeState =3){
-  xmotion.Backward(100,400); 
-  xmotion.Left0(50,200);
+else if (edgeState ==3){
+  xmotion.Backward(100,50); 
+  xmotion.Right0(100,30);
+  xmotion.Backward(100,300); 
+    xmotion.Left0(100,150);
+
+
   //xmotion.Forward(50,150);
+            Serial.println("avoiding FR");
+
   xmotion.StopMotors(0);
   edgeState = checkEdge();
+
 }
   // back sesnor at edge
-else if (edgeState =2){
+ else if (edgeState ==2){
   // xmotion.Left0(50,200);
   xmotion.Forward(100,400);
+  
   xmotion.StopMotors(0);
   edgeState = checkEdge();
+            Serial.println("avoiding FORWARD");
+
 }
   //front left at edge 
-else if (edgeState = 1){
-  xmotion.Backward(100,400); 
-  xmotion.Left0(50,200);
+ else if (edgeState == 1){
+  xmotion.Backward(100,50); 
+  xmotion.Left0(100,30);
+  xmotion.Backward(100,300); 
+    xmotion.Right0(100,150);
+
   //xmotion.Forward(50,150);
   xmotion.StopMotors(0);
   edgeState = checkEdge();
+            Serial.println("avoiding FL");
+
 
 }
 }
-}
-
-
-
-
-//_______________
-// to test: switch case 
-void avoidEdge(int &edgeState) {
-    while (edgeState != 0) {
-        switch (edgeState) {
-            case 4: // Both front 
-                xmotion.Backward(100, 400);
-                xmotion.Left0(50, 300);
-                xmotion.StopMotors(0);
-                break;
-
-            case 3: // Front right 
-                xmotion.Backward(100, 400);
-                xmotion.Left0(50, 200);
-                xmotion.StopMotors(0);
-                break;
-
-            case 2: // Back 
-                xmotion.Forward(100, 400);
-                xmotion.StopMotors(0);
-                break;
-
-            case 1: // Front left 
-                xmotion.Backward(100, 400);
-                xmotion.Right0(50, 200);
-                xmotion.StopMotors(0);
-                break;
-        }
-
-       
-        edgeState = checkEdge();
-    }
-}
-
+ }
 
