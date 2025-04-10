6ddf4b7ffbd2137b2b977d62af37a1285bfdd5b3
Taenam alzamil <92439253+taynamghz@users.noreply.github.com>
2025-04-10 17:47:36 +0300

    Update attack.ino

--- 2025/methods/attack.ino
+++ 2025/methods/attack.ino
@@ -1,37 +1,42 @@
 
 void attack(int direction) {
-  if (direction == 1) { //oponent is infront 
+  if (direction == 1) { // Opponent is in front
     forward_attack();
-    Serial.println("attacking forward");
-  } else if (direction == 5) { //Right forward
-    xmotion.Right0(50, 80);
+    Serial.println("Attacking forward");
+  } else if (direction == 5) { // Right forward
+    xmotion.Right0(50, 70);
+            xmotion.StopMotors(stop);
+
     forward_attack();
     xmotion.StopMotors(30);
-    Serial.println("attacking RF");
-  } else if (direction == 4) { //left forward
-    xmotion.Left0(50,80);
-    forward_attack(); 
+    Serial.println("Attacking RF");
+  } else if (direction == 4) { // Left forward
+    xmotion.Left0(50, 70);
+        xmotion.StopMotors(stop);
+    forward_attack();
     xmotion.StopMotors(30);
-    Serial.println("attacking LF");
-  } else if (direction == 2) {
-    xmotion.Left0(50, 220);
+    Serial.println("Attacking LF");
+  } else if (direction == 2) { // Left
+    xmotion.Left0(50, 150);
+    xmotion.StopMotors(stop);
+
     forward_attack();
     xmotion.StopMotors(100);
-    Serial.println("attacking L");
-  } else if (direction == 3) {
-    xmotion.Right0(50, 220);
+    Serial.println("Attacking L");
+  } else if (direction == 3) { // Right
+    xmotion.Right0(50, 150);
+            xmotion.StopMotors(stop);
+
     forward_attack();
     xmotion.StopMotors(100);
-    Serial.println("attacking R");
+    Serial.println("Attacking R");
   } else {
     xmotion.StopMotors(0);
-    Serial.println("NO ACTION");
+    Serial.println("No action");
   }
-  
-  
+  delay(20);
   int edgeState = checkEdge();
   if (edgeState != 0) {
-    // If edge is detected, avoid it
     avoidEdge(edgeState);
   }
 }
